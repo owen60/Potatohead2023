3cbd35724e72ce5a969eacecda1dce0d077b8d18
Justin Riley <20hz20khz@gmail.com>
2023-10-10 09:57:17 -0400

    Create Potatohead.java

---
+++ Potatohead.java
@@ -0,0 +1,64 @@
+/**
+A class that describes an individual Potatohead toy
+*/
+public class Potatohead
+{
+
+	/*  PRIVATE instance variables go here  */
+	private int eyeNumber;
+	
+	// STEP 1 OF 5: add one instance variable HERE
+	
+  
+	/*  the no argument constructor  */
+	public Potatohead()
+	{
+		/* initialize ***ALL*** instance variables here. */
+		/* Do NOT set Strings to "" or null. */
+		/* Do NOT set Objects to null */
+		eyeNumber = 2;
+    // STEP 2 OF 5: initialize your instance variable HERE
+
+	}
+  
+
+	/*  VARIOUS ACCESSOR AND MUTATOR METHODS IN ALPHABETICAL ORDER GO HERE  */
+	
+  // STEP 3 OF 5: Add an accessor method for yr instance var WITH JAVADOC COMMENT
+  // STEP 4 OF 5: a mutator method for yr instance var WITH JAVADOC COMMENT
+	// !!! NOTE: You MUST add Javadoc comments for your accessor method and mutator method
+	
+		
+	/**
+		void setNumOfEyes(int _numOfEyes)
+		mutator method for eyeNumber
+		@param _numOfEyes for what the user desires.  
+	*/	
+	}
+	public void setNumOfEyes(int _numOfEyes)
+  {
+		eyeNumber = _numOfEyes;
+	}
+		
+	/**
+		String getNumOfEyes()
+		accessor method for eyeNumber
+		@return eyeNumber an int describing the number of eyes.
+	*/
+	public int getNumOfEyes()
+  {
+		return eyeNumber;
+	}
+	
+
+	
+	public String toString()
+	{
+		String output = "PotatoHead object. ";
+		output += "Number Of Eyes: " + eyeNumber + ". ";
+    // STEP 5 OF 5: Add your instance variable to the toString() method using += HERE
+
+		
+		return output;
+	}
+}
